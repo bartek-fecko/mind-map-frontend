077bc809491d2915b4d8ac29e22bf2b1b4257c05
bfecko <bartlomiej.fecko@gmail.com>
2025-06-07 23:55:46 +0200

    commit #6

--- app/providers/SocketSingleton.ts
+++ app/providers/SocketSingleton.ts
@@ -2,10 +2,12 @@ import { io, Socket } from 'socket.io-client';
 
 let socket: Socket | null = null;
 
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+
 export function getSocketSingleton(): Socket {
   if (!socket) {
     if (typeof window !== 'undefined') {
-      socket = io('http://localhost:3001', {
+      socket = io(SOCKET_URL, {
         transports: ['websocket'],
       });
     }
