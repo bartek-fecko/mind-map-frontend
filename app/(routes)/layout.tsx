3edc286afa7d48e2c0bd2faf2e3b0f998d4d2acd
bfecko <bartlomiej.fecko@gmail.com>
2025-06-26 22:50:08 +0200

    commit #15

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Mind Map - visualize your thoughts',
+  title: 'Mind Map - Visualize Your Thoughts',
   description: 'Create anything you imagine',
 };
 
